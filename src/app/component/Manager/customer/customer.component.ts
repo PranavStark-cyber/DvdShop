27159cadcd745c743b938d99ea063c451201dec6
PranavStark-cyber <PranavStark-cyber>
2024-11-30 12:02:01 +0530

    some change

--- src/app/component/Manager/customer/customer.component.ts
+++ src/app/component/Manager/customer/customer.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-customer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './customer.component.html',
   styleUrl: './customer.component.css'
 })
 export class CustomerComponent {
+dvds: any;
 
 }
